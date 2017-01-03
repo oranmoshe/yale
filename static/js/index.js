fa5ea29a43dd5ec5add4f6ff808601ca4a8e76b6
Oran <Oran.moshe@gmail.com>
2017-01-04 00:57:36 +0200

    client design

--- static/js/index.js
+++ static/js/index.js
@@ -15,51 +15,42 @@ var ICON = null;
 var IMAGE = null;
 var CREDIT = null;
 
+function run(){
+	var vocabulary = ["car","dog","sun"];
+	var index = 0;
+	setInterval(function(){
+		generateCommercial(vocabulary[index++]);
+		if(index==vocabulary.length)
+			index=0;
+	}, 3000);
+
+}
+
 $(document).ready(function(){
 	$('#submit').click(function init(){
-		$.getJSON('https://yale-oran-moshe.herokuapp.com/product/'+ $('#term').val() +'?callback=non', 
-		function(json) {
-				console.log(json);
-				$(QUOT).html(json["quot"]) 
-				$(ICON).html(json["icon_svg"])
-				$(IMAGE).html('<img src='+json["image_url"]+' style=\"width:100%\">')
-				$(CREDIT).html(json["image_credit"])
-		  		//imageColors(json["image_url"]);
-		});
-		// //init();
+		generateCommercial($('#term').val());
 	});
 });
+
+function generateCommercial(term){
+	$.getJSON('https://yale-oran-moshe.herokuapp.com/product/'+ term +'?callback=non', 
+	function(json) {
+		$(QUOT).html(json["quot"]) 
+		$(ICON).html(json["icon_svg"])
+		$(CREDIT).html(json["image_credit"])
+		if(json["image_url"]!=""){
+  			imageColors(json["image_url"]);
+  			$(IMAGE).css('background-image','url('+json["image_url"]+')');
+		}else{
+			imageColors('https://images.unsplash.com/photo-1450101499163-c8848c66ca85?dpr=2&auto=format&fit=crop&w=1500&h=1001&q=80&cs=tinysrgb&crop=');
+  			$(IMAGE).html('<img src="https://images.unsplash.com/photo-1450101499163-c8848c66ca85?dpr=2&auto=format&fit=crop&w=1500&h=1001&q=80&cs=tinysrgb&crop=" style=\"width:100%\">')
+		}
+	});
+}
 
 function imageColors(image_url){
 	var colorThief = new ColorThief();
 	colorThief.getColorsAsync(image_url,function(color, element){
-        $('.dominant').css('color','rgb('+color[0][0]+','+color[0][1]+','+color[0][2]+')');
-        $('.color1').css('color','rgb('+color[1][0]+','+color[1][1]+','+color[1][2]+')');
-        $('.color2').css('color','rgb('+color[2][0]+','+color[2][1]+','+color[2][2]+')');
-        $('.color3').css('color','rgb('+color[3][0]+','+color[3][1]+','+color[3][2]+')');
-        $('.color4').css('color','rgb('+color[4][0]+','+color[4][1]+','+color[4][2]+')');
-        $('.color5').css('color','rgb('+color[5][0]+','+color[5][1]+','+color[5][2]+')');
-        $('.color6').css('color','rgb('+color[6][0]+','+color[6][1]+','+color[6][2]+')');
-        $('.color7').css('color','rgb('+color[7][0]+','+color[7][1]+','+color[7][2]+')');
-        $('.color8').css('color','rgb('+color[8][0]+','+color[8][1]+','+color[8][2]+')');
-        $('.color9').css('color','rgb('+color[9][0]+','+color[9][1]+','+color[9][2]+')');
-        $('.box.dominant').css('background-color','rgb('+color[0][0]+','+color[0][1]+','+color[0][2]+')');
-        $('.box.color1').css('background-color','rgb('+color[1][0]+','+color[1][1]+','+color[1][2]+')');
-        $('.box.color2').css('background-color','rgb('+color[2][0]+','+color[2][1]+','+color[2][2]+')');
-        $('.box.color3').css('background-color','rgb('+color[3][0]+','+color[3][1]+','+color[3][2]+')');
-        $('.box.color4').css('background-color','rgb('+color[4][0]+','+color[4][1]+','+color[4][2]+')');
-        $('.box.color5').css('background-color','rgb('+color[5][0]+','+color[5][1]+','+color[5][2]+')');
-        $('.box.color6').css('background-color','rgb('+color[6][0]+','+color[6][1]+','+color[6][2]+')');
-        $('.box.color7').css('background-color','rgb('+color[7][0]+','+color[7][1]+','+color[7][2]+')');
-        $('.box.color8').css('background-color','rgb('+color[8][0]+','+color[8][1]+','+color[8][2]+')');
-        $('.box.color9').css('background-color','rgb('+color[9][0]+','+color[9][1]+','+color[9][2]+')');
-        console.log(color);
-	});
-}
-
-function loadImage(url){
-	var colorThief = new ColorThief();
-	colorThief.getColorAsync('http://images.wcdn.co.il/2301527-54.jpg?crop=w635&output=webp',function(color, element){
 		COLOR_DOMINANT = 'rgb('+color[0][0]+','+color[0][1]+','+color[0][2]+')';
 		COLOR_1 = 'rgb('+color[1][0]+','+color[1][1]+','+color[1][2]+')';
 		COLOR_2 = 'rgb('+color[2][0]+','+color[2][1]+','+color[2][2]+')';
@@ -70,6 +61,8 @@ function loadImage(url){
 		COLOR_7 = 'rgb('+color[7][0]+','+color[7][1]+','+color[7][2]+')';
 		COLOR_8 = 'rgb('+color[8][0]+','+color[8][1]+','+color[8][2]+')';
 		COLOR_9 = 'rgb('+color[9][0]+','+color[9][1]+','+color[9][2]+')';
+        initElementsColors();
 	});
 }
+
 
